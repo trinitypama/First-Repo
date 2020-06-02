aca41fc68ae15d5c6fbf1af5292dc627a0204ed8
Trinity Pama <trinitypama@gmail.com>
2020-06-02 18:05:00 -0400

    Commit Sample Application

---
+++ BooksApi/BooksApi/Services/BookService.cs
@@ -0,0 +1,46 @@
+﻿using BooksApi.Models;
+using BooksApi.Settings;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+        private readonly string _table;
+
+        public BookService(IBookstoreDBSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _table = "Books";
+
+            _books = database.GetCollection<Book>(_table);
+        }
+
+        public List<Book> Get() =>
+            _books.Find(book => true).ToList();
+
+        public Book Get(string id) =>
+            _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn) =>
+            _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(Book bookIn) =>
+            _books.DeleteOne(book => book.Id == bookIn.Id);
+
+        public void Remove(string id) =>
+            _books.DeleteOne(book => book.Id == id);
+    }
+}
---
+++ BooksApi/BooksApi/Settings/BookstoreDBSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace BooksApi.Settings
+{
+    public class BookstoreDBSettings : IBookstoreDBSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IBookstoreDBSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
